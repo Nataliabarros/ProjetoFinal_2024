cafc83ae090699541684816ef6929d716bdf6d12
Natália Lima <47815896+Nataliabarros@users.noreply.github.com>
2024-08-06 15:54:15 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+
+
+//import Message from './components/Message';
+//import Teste from './components/Teste';
+import Crud from './components/Crud';
+
+
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Projeto final WEB 2024</h1>
+                
+                <Crud/>
+            </header>
+        </div>
+    );
+}
+
+export default App;
